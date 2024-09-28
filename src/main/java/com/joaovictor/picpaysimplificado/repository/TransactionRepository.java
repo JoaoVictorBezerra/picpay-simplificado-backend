2f0d269106747b6bdad822b52811588eb5ed54f6
João Victor Bezerra da Silva <dev.joaovictor@hotmail.com>
2024-09-28 17:19:08 +0100

    fix: add repository annotation

--- src/main/java/com/joaovictor/picpaysimplificado/repository/TransactionRepository.java
+++ src/main/java/com/joaovictor/picpaysimplificado/repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package com.joaovictor.picpaysimplificado.repository;
 
 import com.joaovictor.picpaysimplificado.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
