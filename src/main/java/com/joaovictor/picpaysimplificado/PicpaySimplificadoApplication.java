9d546c58f457283e4957d0ce3d9c7bb9b936eef9
João Victor Bezerra da Silva <dev.joaovictor@hotmail.com>
2024-09-28 16:22:44 +0100

    feat: add enable schedule annotation

--- src/main/java/com/joaovictor/picpaysimplificado/PicpaySimplificadoApplication.java
+++ src/main/java/com/joaovictor/picpaysimplificado/PicpaySimplificadoApplication.java
@@ -2,8 +2,10 @@ package com.joaovictor.picpaysimplificado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PicpaySimplificadoApplication {
 
     public static void main(String[] args) {
